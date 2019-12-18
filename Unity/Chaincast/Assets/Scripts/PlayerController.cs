4af0e3af9ff7cbf6e8f69ef275543d52a81b035d
Aaron Wilson <aaron.wi@gmail.com>
2019-12-18 07:22:12 +0000

    added a tilemap with auto 2d collider generation + added basic movement to the player (normalized to prevent superfast diagonal movement).

---
+++ Unity/Chaincast/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float maxSpeed = 5f;
+
+    Rigidbody2D rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        if( rb == null )
+            Debug.LogError( "no rigid body in player" );
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis( "Horizontal" );
+        float verticalInput = Input.GetAxis( "Vertical" );
+
+        Vector2 inputVector = new Vector2(horizontalInput, verticalInput);
+        Vector2 speedVector = inputVector * maxSpeed;
+
+        Vector2 normalisedInputVector = inputVector.normalized;
+        Vector2 fixedSpeedVector = new Vector2(speedVector.x * Mathf.Abs(normalisedInputVector.x), speedVector.y * Mathf.Abs(normalisedInputVector.y));
+
+        //bool horizontalThresholdMet = Mathf.Abs( horizontalInput ) > 0.1f;
+        //bool verticalThresholdMet = Mathf.Abs( verticalInput ) > 0.1f;
+
+        //if( horizontalThresholdMet )
+            //finalSpeedVector.x = speedVector.x;
+
+        //if( verticalThresholdMet )
+            //finalSpeedVector.y = speedVector.y;
+
+        rb.velocity = fixedSpeedVector;
+    }
+}
