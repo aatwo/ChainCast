f9ecbd68438e56efde31686f759a1c885bd10906
Aaron Wilson <aaron.wi@gmail.com>
2019-12-19 07:34:08 +0000

    four players are now spawned and the camera is attached to player1

--- Unity/Chaincast/Assets/Scripts/PlayerController.cs
+++ Unity/Chaincast/Assets/Scripts/PlayerController.cs
@@ -7,7 +7,12 @@ public class PlayerController : MonoBehaviour
     public float maxSpeed = 5f;
 
     Rigidbody2D rb;
+    int playerIndex = 0;
 
+    public void SetPlayerIndex(int n)
+    {
+        playerIndex = n;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -15,18 +20,22 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         if( rb == null )
             Debug.LogError( "no rigid body in player" );
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis( "Horizontal" );
-        float verticalInput = Input.GetAxis( "Vertical" );
+        if( playerIndex == 0 )
+        {
+            float horizontalInput = Input.GetAxis( "Horizontal" );
+            float verticalInput = Input.GetAxis( "Vertical" );
 
-        Vector2 normalisedInputVector = (new Vector2(horizontalInput, verticalInput)).normalized;
-        float xVel = horizontalInput * maxSpeed * Mathf.Abs(normalisedInputVector.x);
-        float yVel = verticalInput * maxSpeed * Mathf.Abs(normalisedInputVector.y);
+            Vector2 normalisedInputVector = (new Vector2(horizontalInput, verticalInput)).normalized;
+            float xVel = horizontalInput * maxSpeed * Mathf.Abs(normalisedInputVector.x);
+            float yVel = verticalInput * maxSpeed * Mathf.Abs(normalisedInputVector.y);
 
-        rb.velocity = new Vector2(xVel, yVel);
+            rb.velocity = new Vector2( xVel, yVel );
+        }
     }
 }
